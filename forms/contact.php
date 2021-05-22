c0de2ae992042049ff450e71492da5ae149da501
Gianfranco Groppo <gf.groppo@gmail.com>
2021-05-22 20:31:52 -0300

    nuevas mod para movil

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,35 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+$remitente = 'abogados@estudiolopezamaya.com.ar';
+$destinatario = 'abogados@estudiolopezamaya.com.ar'; // en esta línea va el mail del destinatario.
+$asunto = 'Consulta desde sitio web'; // acá se puede modificar el asunto del mail
+if (!$_POST){
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+?>
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+<?php
+}else{
+	 
+    $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo .= "Telefono: " . $_POST["telefono"] . "\r\n";
+  	$cuerpo .= "Consulta: " . $_POST["mensaje"] . "\r\n";
+	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+	// Si se agrega un campo al formulario, hay que agregarlo acá.
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
 
-  echo $contact->send();
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'confirma.html'; //se debe crear un html que confirma el envío
+}
+
+
+
+
+
+
 ?>
